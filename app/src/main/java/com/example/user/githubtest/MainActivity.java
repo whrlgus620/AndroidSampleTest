708e42e7378e4dea38ec3b8ce8d379fbba330ff5
whrlgus620 <36057697+whrlgus620@users.noreply.github.com>
2018-02-06 15:04:31 +0900

    Update MainActivity.java
    Change file

--- app/src/main/java/com/example/user/githubtest/MainActivity.java
+++ app/src/main/java/com/example/user/githubtest/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);   //gegw
+        setContentView(R.layout.activity_main);   //2018.02.06
     }
 }
